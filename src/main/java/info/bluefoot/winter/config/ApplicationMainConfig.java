865693338eb2bd651a67f37d722f6b836c20b3dc
gewton <gjhames@gmail.com>
2016-06-07 18:13:39 -0300

    fetching db connection info from env vars

--- src/main/java/info/bluefoot/winter/config/ApplicationMainConfig.java
+++ src/main/java/info/bluefoot/winter/config/ApplicationMainConfig.java
@@ -18,7 +18,9 @@ package info.bluefoot.winter.config;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
@@ -33,11 +35,26 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 @EnableScheduling
 public class ApplicationMainConfig {
     
+    @Value("#{systemEnvironment['WINTER_DB_HOST']}")
+    private String databaseHost;
+
+    @Value("#{systemEnvironment['WINTER_DB_USER']}")
+    private String databaseUser;
+    
+    @Value("#{systemEnvironment['WINTER_DB_PASSWD']}")
+    private String databasePassword;
+    
     @Bean(destroyMethod="close", autowire=Autowire.BY_TYPE)
     public DataSource dataSource() throws Throwable {
+        String envVar = System.getenv("WINTER_DB_USER");
+
+        if(StringUtils.isBlank(databaseHost)) databaseHost = "127.0.0.1";
+        if(StringUtils.isBlank(databaseUser) || StringUtils.isBlank(databasePassword)) {
+            throw new IllegalStateException("Can't find out database user and pwd based on environment variables");
+        }
         com.mchange.v2.c3p0.ComboPooledDataSource ds = new com.mchange.v2.c3p0.ComboPooledDataSource();
         ds.setDriverClass("org.postgresql.Driver");
-        ds.setJdbcUrl("jdbc:postgresql://127.8.229.130:5432/winter?user=adminmqswr7r&password=aVIYl1CYY_rE"); //prod: 127.8.229.130 // dev: 127.0.0.1
+        ds.setJdbcUrl("jdbc:postgresql://" + databaseHost + ":5432/winter?user=" + databaseUser + "&password=" + databasePassword); //prod: 127.8.229.130 // dev: 127.0.0.1
         ds.setMaxPoolSize(40);
         ds.setMinPoolSize(1);
         ds.setMaxIdleTime(200);
